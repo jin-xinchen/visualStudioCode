527389be19e21b0af3bb75b6c14d6b098dfa1d89
jin-xinchen <jinxinchen@ymail.com>
2018-06-22 15:19:23 -0400

    using System.ComponentModel.DataAnnotations.Schema;
    using System.ComponentModel.DataAnnotations.Schema;

--- TodoApi/Models/Branch01.cs
+++ TodoApi/Models/Branch01.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations.Schema;
 using Newtonsoft.Json;
 
 public class Branch01
     {
         [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string Message { get; }
+        [Column("age_restriction")]
+        public string Age { get; set; }
+        [Column("name")]
+        public string Name { get; set; }
     }
