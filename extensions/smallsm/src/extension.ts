32fe4385aebffe00bff9be0936e31510fc0b6450
jin <jinxinchen@ymail.com>
2018-07-13 12:41:08 -0400

    WordCounter class

--- extensions/smallsm/src/extension.ts
+++ extensions/smallsm/src/extension.ts
@@ -1,37 +1,86 @@
 'use strict';
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import {window, commands, Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument} from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "smallsm" is now active!');
 
+    // create a new word counter
+    let wordCounter = new WordCounter();
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-        let editor = vscode.window.activeTextEditor;
-        if (!editor) {
-            return;
-        }
-        else {
-            let selection = editor.selection;
-            let text = editor.document.getText(selection);
-            vscode.window.showInformationMessage("" + text.length);
-        }
+    let disposable = commands.registerCommand('extension.sayHello', () => {
+        wordCounter.updateWordCount();
+        // let editor = window.activeTextEditor;
+        // if (!editor) {
+        //     return;
+        // }
+        // else {
+        //     let selection = editor.selection;
+        //     let text = editor.document.getText(selection);
+        //     window.showInformationMessage("" + text.length);
+        // }
         // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        // window.showInformationMessage('Hello World!');
     });
-
+    context.subscriptions.push(wordCounter);
     context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {
+}
+
+class WordCounter {
+
+    private _statusBarItem: StatusBarItem =  window.createStatusBarItem(StatusBarAlignment.Left);
+
+    public updateWordCount() {
+
+        // Get the current text editor
+        let editor = window.activeTextEditor;
+        if (!editor) {
+            this._statusBarItem.hide();
+            return;
+        }
+
+        let doc = editor.document;
+
+        // Only update status if an Markdown file
+        if (doc.languageId === "markdown") {
+            let wordCount = this._getWordCount(doc);
+
+            // Update the status bar
+            this._statusBarItem.text = wordCount !== 1 ? `${wordCount} Words` : '1 Word';
+            this._statusBarItem.show();
+        } else {
+            this._statusBarItem.hide();
+        }
+    }
+
+    public _getWordCount(doc: TextDocument): number {
+
+        let docContent = doc.getText();
+
+        // Parse out unwanted whitespace so the split is accurate
+        docContent = docContent.replace(/(< ([^>]+)<)/g, '').replace(/\s+/g, ' ');
+        docContent = docContent.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+        let wordCount = 0;
+        if (docContent !== "") {
+            wordCount = docContent.split(" ").length;
+        }
+
+        return wordCount;
+    }
+
+    dispose() {
+        this._statusBarItem.dispose();
+    }
 }
